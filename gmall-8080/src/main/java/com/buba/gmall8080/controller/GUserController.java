68c61a96d2f5606b0f46dd1605e49aa8addd3f3b
jia <jia417779025@qq.com>
2020-04-12 23:05:30 +0800

    fengzhaoyang --- 我提交了test

--- gmall-8080/src/main/java/com/buba/gmall8080/controller/GUserController.java
+++ gmall-8080/src/main/java/com/buba/gmall8080/controller/GUserController.java
@@ -1,9 +1,17 @@
 package com.buba.gmall8080.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.buba.gmall8080.pojo.GGroup;
+import com.buba.gmall8080.pojo.GUser;
+import com.buba.gmall8080.service.GUserService;
+import com.buba.gmall8080.utils.Result;
+import com.buba.gmall8080.utils.StatusCode;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,20 @@ import org.springframework.stereotype.Controller;
  * @author MrJia
  * @since 2020-04-12
  */
-@Controller
+@RestController
+@CrossOrigin
 @RequestMapping("/gUser")
 public class GUserController {
+    @Resource
+    private GUserService gUserService;
+    @GetMapping("/getLogin")
+    public Result getLogin(@RequestBody GUser gUser){
+        QueryWrapper<GUser>www= new QueryWrapper<>();//查询条件对象
+        //参数赋值
+        www.eq("userAccount",gUser.getUserAccount()).eq("userPasswd",gUser.getUserPasswd());
+        GUser one = gUserService.getOne(www);
+        return new Result(true, StatusCode.OK,"登陆成功",one);
+    }
 
 }
 
---
+++ gmall-8080/src/main/java/com/buba/gmall8080/service/impl/GUserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.buba.gmall8080.service.impl;
+import com.buba.gmall8080.pojo.GUser;
+import com.buba.gmall8080.mapper.GUserMapper;
+import com.buba.gmall8080.service.GUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author MrJia
+ * @since 2020-04-12
+ */
+@Service
+public class GUserServiceImpl extends ServiceImpl<GUserMapper, GUser> implements GUserService {
+
+}
